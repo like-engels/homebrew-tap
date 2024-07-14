e893786641000f16a14cb28ee7a6761679ba6c32
Engels Ruehl <higashikataengels@icloud.com>
2024-07-13 18:51:13 -0600

    feat: update amber sources to target 0.3.4 release

--- Formula/amber-bash.rb
+++ Formula/amber-bash.rb
@@ -1,8 +1,8 @@
-class AmberLang < Formula
+class AmberBash < Formula
   desc "Amber, the programming language that compiles to Bash"
   homepage "https://amber-lang.com"
-  url "https://github.com/Ph0enixKM/Amber/releases/download/0.3.3-alpha/source.tar.gz"
-  sha256 "54a4f681f8ea1b19543ffa1569203c3bff88ef8ffe73422424f65f8be3cf5cd0"
+  url "https://github.com/Ph0enixKM/Amber/releases/download/0.3.4-alpha/source.tar.gz"
+  sha256 "26da447d26b1c502b87055fdaae3a62f443e4f0d1c8866e6dca995ee664cc3b4"
   license "GPL-3.0-only"
 
   depends_on "rust" => :build
@@ -13,9 +13,9 @@ class AmberLang < Formula
 
   def caveats
     <<~EOS
-      Amber programs need Bash to run. macOS and most Linux distributions ship with it preinstalled, but some (Alpine, for instance) do not.
+      Programs written in amber require Bash to run. macOS and most Linux distributions ship with bash preinstalled, but some (Alpine, for instance) do not.
 
-      In order to run built programs, please make sure you have Bash installed.
+      In order to run built programs, please make sure you have Bash already installed.
     EOS
   end
 
